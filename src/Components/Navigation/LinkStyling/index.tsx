5df2fff49a942a50502f15e918598575f840a6a2
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-09-21 16:50:57 -0500

    added navigation icons

--- src/Components/Navigation/LinkStyling/index.tsx
+++ src/Components/Navigation/LinkStyling/index.tsx
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom'
 import { Menu, Divider } from 'antd'
 import Sider from 'antd/es/layout/Sider'
 import { FaRegFaceGrinTongueWink } from 'react-icons/fa6'
+import {AiOutlineHome, AiOutlineCalendar, AiOutlineQuestionCircle, AiOutlineMail, AiOutlineSetting, AiOutlineMenu} from 'react-icons/ai'
+import {MdOutlineDashboard, MdOutlineManageAccounts, MdOutlineSupportAgent} from 'react-icons/md'
+import {FiUsers, FiMessageSquare, FiLogOut} from 'react-icons/fi'
 
 const LinkStyling = () => {
 return (
     <Sider>
-    <h1>WELCOME</h1>
+    <h1 className="welcome">WELCOME</h1>
     <Divider />
     <div className="profile-pic">
         <FaRegFaceGrinTongueWink />
@@ -18,50 +21,60 @@ return (
     </div>
     <Divider />
     <Menu theme="dark" mode="inline">
+        <AiOutlineMenu/>
         <h1>Menu:</h1>
         <Menu.Item key="1">
-          {/* <Icon type="pie-chart" /> */}
+        <AiOutlineHome/>
         <span>Home</span>
         <Link to="/" />
         </Menu.Item>
         <Menu.Item key="2">
-          {/* <Icon type=""/> */}
+        <MdOutlineDashboard/>
         <span>Dashboard</span>
         <Link to="/dashboard" />
         </Menu.Item>
         <Menu.Item key="3">
+        <FiUsers/>
         <span>Users</span>
         <Link to="/users" />
         </Menu.Item>
         <Menu.Item key="4">
+        <FiMessageSquare/>
         <span>Conversations</span>
         <Link to="/conversations" />
         </Menu.Item>
         <Menu.Item key="5">
+        <AiOutlineCalendar/>
         <span>Calendar</span>
         <Link to="/calendar" />
         </Menu.Item>
     </Menu>
     <Divider />
     <Menu theme="dark" mode="inline">
+        <MdOutlineSupportAgent/>
         <h1>Support:</h1>
         <Menu.Item key="6">
+        <AiOutlineQuestionCircle/>
         <span>Need Help?</span>
           {/* <Link to="/conversations"/> */}
         </Menu.Item>
         <Menu.Item key="7">
+        <AiOutlineMail/>
         <span>Contact Us</span>
           {/* <Link to="/calendar"/> */}
         </Menu.Item>
     </Menu>
     <Divider />
     <Menu theme="dark" mode="inline">
+        <AiOutlineSetting/>
         <h1>Settings:</h1>
         <Menu.Item key="8">
+        <MdOutlineManageAccounts/>
         <span>Account</span>
           {/* <Link to="/"/> */}
         </Menu.Item>
         <Menu.Item key="9">
+        <FiLogOut/>
         <span>Logout</span>
           {/* <Link to="/"/> */}
         </Menu.Item>
