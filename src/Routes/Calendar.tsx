9236db656fb76eb7f3f5402ab95ccdc8052398c1
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-10-12 17:03:32 -0500

    working on calendar component

--- src/Routes/Calendar.tsx
+++ src/Routes/Calendar.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import type { Dayjs } from 'dayjs'
+import { Calendar} from 'antd'
+import type { CalendarProps } from 'antd'
+
+//TODO add a reminders section also
 
 const Calendar = () => {
-  return <div>Calendar //TODO add a reminders section also
-    
-  </div>
+  const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
+    console.log("Calendar Value:", value.format('YYYY-MM-DD'), mode)
+  }
+  return (
+    <Calendar onPanelChange={onPanelChange} />
+
+  )
 }
 
 export default Calendar
